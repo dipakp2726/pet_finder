fd529a3fa45c801f83271f0cf860cd541d715d2a
Dipak <dipakp2726@gmail.com>
2022-12-27 16:57:20 +0530

    added configs and style

---
+++ lib/core/configs/configs.dart
@@ -0,0 +1,13 @@
+/// App level configuration variables
+class Configs {
+
+  /// Base API URL of The petfinder API
+  ///
+  /// See: https://www.petfinder.com/developers/v2/docs/
+  static const String apiBaseUrl = 'https://api.petfinder.com/v2';
+
+  /// API Key registered with The petfinder API
+  ///
+  /// See: https://www.petfinder.com/developers/v2/docs/
+  static const String petAPIKey = String.fromEnvironment('PET_API_KEY');
+}
---
+++ lib/core/configs/styles/ui_constants.dart
@@ -0,0 +1,5 @@
+/// Class holding constants used in UI code
+class UIConstants {
+  /// Height of a list item in a pet list
+  static const double petListItemHeight = 110;
+}
