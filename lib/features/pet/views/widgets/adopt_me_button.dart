d2c45f3ba3bed00ef2031d74b6407485c73a77d9
Dipak <dipakp2726@gmail.com>
2022-12-29 11:00:15 +0530

    display the popup with a confetti on adpot pet

--- lib/features/pet/views/widgets/adopt_me_button.dart
+++ lib/features/pet/views/widgets/adopt_me_button.dart
@@ -1,46 +1,94 @@
+import 'package:confetti/confetti.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_heaven/core/configs/styles/app_colors.dart';
 import 'package:pet_heaven/core/widgets/animate_in_effect.dart';
 import 'package:pet_heaven/features/pet/providers/adoption_provider.dart';
 
-class AdoptMeButton extends ConsumerWidget {
+class AdoptMeButton extends ConsumerStatefulWidget {
   const AdoptMeButton({
+    super.key,
     required this.petId,
     required this.petName,
-    super.key,
   });
 
   final int petId;
   final String petName;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final isAdopted = ref.watch(isPetAdpotedProvider(petId: petId));
+  ConsumerState<AdoptMeButton> createState() => _AdoptState();
+}
 
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: SizedBox(
-        height: 55,
-        width: double.infinity,
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            // foregroundColor: AppColors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
+class _AdoptState extends ConsumerState<AdoptMeButton> {
+  late ConfettiController _controllerCenter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controllerCenter =
+        ConfettiController(duration: const Duration(milliseconds: 200));
+  }
+
+  @override
+  void dispose() {
+    _controllerCenter.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isAdopted = ref.watch(isPetAdpotedProvider(petId: widget.petId));
+
+    return Stack(
+      children: [
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: SizedBox(
+              height: 55,
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  // foregroundColor: AppColors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  textStyle: Theme.of(context).textTheme.headline2,
+                ),
+                onPressed: !isAdopted
+                    ? () {
+                        ref.read(AdoptPetProvider(petId: widget.petId));
+
+                        _controllerCenter.play();
+                        showSuccessDialog(context, widget.petName);
+                      }
+                    : null,
+                child: Text(isAdopted ? 'Already Adopted' : 'Adopt Me'),
+              ),
             ),
-            textStyle: Theme.of(context).textTheme.headline2,
           ),
-          onPressed: !isAdopted
-              ? () {
-                  ref.read(AdoptPetProvider(petId: petId));
-
-                  showSuccessDialog(context, petName);
-                }
-              : null,
-          child: Text(isAdopted ? 'Already Adopted' : 'Adopt Me'),
         ),
-      ),
+        Align(
+          alignment: Alignment.center,
+          child: ConfettiWidget(
+            confettiController: _controllerCenter,
+            numberOfParticles: 200,
+            blastDirectionality: BlastDirectionality.explosive,
+
+            shouldLoop: false,
+            colors: const [
+              Colors.green,
+              Colors.blue,
+              Colors.pink,
+              Colors.orange,
+              Colors.purple
+            ], // manually specify the colors to be used
+            // createParticlePath: drawStar, // define a custom shape/path.
+          ),
+        ),
+      ],
     );
   }
 
@@ -51,11 +99,11 @@ class AdoptMeButton extends ConsumerWidget {
         child: AlertDialog(
           title: Text(
             'Congratulations',
-            style: Theme.of(context).primaryTextTheme.headline2,
+            style: Theme.of(context).textTheme.headline2,
           ),
           content: Text(
             'You’ve now adopted $petName',
-            style: Theme.of(context).primaryTextTheme.headline4,
+            style: Theme.of(context).textTheme.headline4,
           ),
           actions: [
             ElevatedButton(
